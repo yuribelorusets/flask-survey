8326f21e8034687c67d75ad2cfc9d0897dd1949e
Yuri <yuri.belorusets@gmail.com>
2022-03-24 17:05:02 -0700

    completed up to code review

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from importlib import resources
 from flask import Flask, request, render_template, redirect, flash, session, template_rendered
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
@@ -9,18 +10,45 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
-survey_title = survey.title
-survey_instructions = survey.instructions
-questions = survey.questions
+
+# survey_title = survey.title
+# survey_instructions = survey.instructions
+# questions = survey.questions
+# q_index = 0
+# question = questions[q_index]
+# choices = []
+
+
+# for question in questions:
+#     choices.append(question.choices)
+
+# choice = choices[q_index]
+
 
 @app.get("/")
 def go_home():
-    return render_template("/survey_start.html", survey_title=survey_title, survey_instructions=survey_instructions)
+    return render_template("/survey_start.html", survey=survey)
 
 @app.post("/begin")
 def start_survey():
-    return redirect("/questions/0")
+    return redirect(f"/questions/0")
 
-@app.get("/questions/0")
-def question_0():
-    return render_template("/question.html", question=questions[0])
+
+@app.get("/questions/<int:index>")
+def display_question(index):
+    index = len(responses)
+    question = survey.questions[index]
+    return render_template("/question.html", survey_question=question)
+
+@app.post("/answer")
+def submit_answer():
+    
+    answer = request.form["answer"]
+    responses.append(answer)
+    index = len(responses)
+
+    if index == len(survey.questions):
+        return render_template("/completion.html")  
+
+    return redirect(f"/questions/{index}")
+
